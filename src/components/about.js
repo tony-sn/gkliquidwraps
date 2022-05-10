2089584dbdb26380e89e6bd7ca8b396ec2b3eba4
tony-sn <tony.sn@outlook.com>
2022-05-11 03:21:10 +0700

    typo fix

---
+++ src/components/about.js
@@ -0,0 +1,51 @@
+const About = () => (
+	<>
+		GK Liquidwraps™ provides the best ​liquid spray wrap&nbsp;application
+		service in Australia since 2021 delivering an amazing service at unbeatable
+		costs. If you are looking to change or protect the colour of your car and
+		time is not on your side, we can&nbsp;dip your car&nbsp;within 1 day in a
+		matte or satin finish. Car dipping is the process to spray wrap your vehicle
+		with a removable paint. Your vehicle original paint is 100% preserved and is
+		not sanded or touched in any ways except for washing. Once you decide to
+		remove the liquid spray wrap, your original paint has been preserved looking
+		brand new.
+		<br />
+		<br />
+		Our premium&nbsp;liquid wrap&nbsp;or liquid&nbsp;spray-on vinyl
+		wrap&nbsp;comes in a wide range of&nbsp;
+		<a href="/colours.html">colours</a>&nbsp;and is sprayed on your car body.
+		Once dry,&nbsp;will almost become like&nbsp;vinyl wrap. Is durable, safe and
+		inexpensive compared to an average $6,000+ standard paint job. Is easy
+		to&nbsp;
+		<a href="/liquid-wrap-after-care.html">maintain</a>, removable only when you
+		want, keeping the value of your car by preserving it and most of all is 100%
+		removable.&nbsp;
+		<br />
+		<br />​<a href="/pricing.html">Liquid wrap pricing</a>
+		&nbsp;starts&nbsp;from as low as $890&nbsp;for small micro cars and the time
+		required to apply is 1 full day. This is the best option for you if you
+		prefer to stand out from the crowd and have a stylish matte or satin finish
+		with a quick turnaround at a very affordable cost.
+		<br />
+		<br />
+		<span>At times, we are able to provide classic vehicle</span>
+		<span>&nbsp;vinyl wrap</span>
+		<span>
+			&nbsp;services either in full or partial.
+			<br />
+			The turnaround for a vehicle
+		</span>
+		<span>&nbsp;wrap (vinyl wrap)</span>
+		<span>
+			&nbsp;is between 2 and 4 days and costs will be double or more than a
+			liquid wrap installation.
+		</span>
+		<br />
+		<br />
+		​This is a great value if you want to protect your original car paint
+		and&nbsp;<strong>increase your resale value</strong>&nbsp;or if you want to
+		make look the old faded paint brand new!&nbsp;
+	</>
+);
+
+export default About;
